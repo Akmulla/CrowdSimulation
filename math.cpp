d46b0fbf685806b66c98712fc1b8b2b1bab6ab16
Akmulla <d4n73@yandex.ru>
2021-11-28 04:27:14 +0300

    basic operation WIP

--- math.cpp
+++ math.cpp
@@ -1,5 +1,11 @@
 #include "math.h"
 
+Vector2::Vector2()
+{
+	this->x = 0;
+	this->y = 0;
+}
+
 Vector2::Vector2(float x, float y)
 {
 	this->x = x;
@@ -14,4 +20,25 @@ Vector2 Vector2::operator+ (const Vector2& right)
 Vector2 Vector2::operator- (const Vector2& right)
 {
 	return Vector2(x - right.x, y - right.y);
+}
+
+float Dot(const Vector2& right)
+{
+
+}
+
+float Vector2::Magnitude()
+{
+	return 0.0f;
+}
+
+
+Vector2 Vector2::Direction()
+{
+	
+}
+
+void Vector2::Normalize()
+{
+	
 }
--- math.h
+++ math.h
@@ -6,9 +6,13 @@ public:
 	float x;
 	float y;
 
+	Vector2();
 	Vector2(float x, float y);
+
 	Vector2 operator+ (const Vector2& right);
 	Vector2 operator- (const Vector2& right);
+	float Magnitude();
+	Vector2 Direction();
+	void Normalize();
+	//float Dot(const Vector2& right);
 };
-
-//Vector2 operator+(const Vector2& left, const Vector2& right);
