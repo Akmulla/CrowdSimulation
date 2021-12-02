e2ceec4bf4d1bbfd10521045a3ec5e9ebed08c31
Akmulla <d4n73@yandex.ru>
2021-12-02 13:33:04 +0300

    sprite class

---
+++ sprite.cpp
@@ -0,0 +1,21 @@
+#include "sprite.h"
+
+Sprite::Sprite()
+{
+
+}
+
+void Sprite::CreateSpriteTexture(const wchar_t* spritePath)
+{
+
+}
+
+void Sprite::SetVertexShader(ID3D11VertexShader* vertex_shader)
+{
+
+}
+
+void Sprite::SetPixelShader(ID3D11PixelShader* pixel_shader)
+{
+
+}
---
+++ sprite.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <d3d11.h>
+
+class Sprite
+{
+public:
+	Sprite();
+	void CreateSpriteTexture(const wchar_t* spritePath);
+	void SetVertexShader(ID3D11VertexShader* vertex_shader);
+	void SetPixelShader(ID3D11PixelShader* pixel_shader);
+private:
+	ID3D11VertexShader* vertex_shader_;
+	ID3D11PixelShader* pixel_shader_;
+
+	ID3D11Resource* sprite_texture_;
+	ID3D11ShaderResourceView* texture_view_;
+	ID3D11SamplerState* sampler_state_;
+};
