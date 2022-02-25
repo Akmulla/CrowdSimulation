e4cbc450bc3fe97a3f708c1ab5e1c8dbfc740f6d
Akmulla <d4n73@yandex.ru>
2022-02-25 20:32:43 +0300

    .

--- sprite.h
+++ sprite.h
@@ -6,7 +6,7 @@ class Sprite
 public:
 	Sprite();
 	~Sprite();
-	void Initialize(ID3D11Resource* sprite_texture, ID3D11ShaderResourceView* texture_view_, ID3D11SamplerState* sampler_state_);
+	void Initialize(ID3D11Resource* sprite_texture, ID3D11ShaderResourceView* texture_view, ID3D11SamplerState* sampler_state);
 	void SetVertexShader(ID3D11VertexShader* vertex_shader);
 	void SetPixelShader(ID3D11PixelShader* pixel_shader);
 private:
